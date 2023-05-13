96750bee5518e89f5eb6d0d876dfa0fac3c43512
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-13 10:56:49 +0200

    Workflow fix

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Test");
     }
 }
