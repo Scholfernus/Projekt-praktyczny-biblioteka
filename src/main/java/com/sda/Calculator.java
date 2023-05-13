eee8675aed449ccd03de703c6f21d26055527583
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-13 11:24:45 +0200

    Calculator

---
+++ src/main/java/com/sda/Calculator.java
@@ -0,0 +1,12 @@
+package com.sda;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+
+    public int calculator(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+}
